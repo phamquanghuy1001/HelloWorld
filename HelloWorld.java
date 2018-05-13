465ba5ea5266cd5cfe85effe3e22203297aa9fb4
phamquanghuy1001 <phamquanghuy1001@gmail.com>
2018-05-13 22:25:11 +0700

    Add date

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
 import java.io.*;
-
+import java.util.Date
 class HelloWorld
 {
     public static void main(String[] args) 
     {
         System.out.println("Hello Pham Quang Huy!");
+		System.out.println("Now: " + new Date());
     }
 }
